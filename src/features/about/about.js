0a4e0ffb074c831356730631ec1196b2e6ff491e
devvydaisy <devvydaisy@gmail.com>
2018-11-08 22:15:04 -0800

    about color and alignment of text

--- src/features/about/about.js
+++ src/features/about/about.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from '../common/header';
 import './about.scss';
 
+
 class About extends Component {
 
 
@@ -9,10 +10,12 @@ class About extends Component {
     return (
       <div className="about-root">
         <Header/>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <h1>hello</h1>
+        <div className="about-box">
+         <img src='../image/about.JPG'></img>
+          <p>Des Devoni uses sustainable practices to help reduce the carbon footprint that the fashion industry leaves.
+            By using remnent textiles we can create unique and one of a kind items that you wont get anywhere else.
+          </p>
+        </div>
       </div>
     );
   }
