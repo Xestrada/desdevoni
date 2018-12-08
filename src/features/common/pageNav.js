62991bb26e6a443859b48cd3293fcb039a7389e7
Xestrada <Xestrada.Jorge@gmail.com>
2018-12-07 18:56:51 -0800

    Organized page navigation

--- src/features/common/pageNav.js
+++ src/features/common/pageNav.js
@@ -13,23 +13,51 @@ export default class PageNav extends React.Component {
     super(props);
 
     this.state = {
-      
+      selected: 0
     };
+
+    this.selectClick = this.selectClick.bind(this);
+  }
+
+  selectClick(value){
+    this.setState({
+      selected: value
+    }, () => {
+      this.props.onClick(value);
+    });
   }
 
   render() {
     return (
       <div className = {`pageNav ${this.props.className}`}>
-        <h5>Portfolio</h5>
-        <h6><a onClick = {() => this.props.onClick(0)}>Festive Fall</a></h6>
-        <h6><a onClick = {() => this.props.onClick(1)}>Sequin Drip Jacket</a></h6>
-        <h6><a onClick = {() => this.props.onClick(2)}>Bratz Evening Wear</a></h6>
-        <h6><a onClick = {() => this.props.onClick(3)}>Remnant Fabric Collection In Process</a></h6>
-        <h6><a onClick = {() => this.props.onClick(4)}>Costume Designer: Hades and Persephone</a></h6>
-        <h6><a onClick = {() => this.props.onClick(5)}>Slow Fashion In Process</a></h6>
-        <h6><a onClick = {() => this.props.onClick(6)}>Science Fiction Collection</a></h6>
-        <h6><a onClick = {() => this.props.onClick(7)}>Leaf Block Print</a></h6>
-        <h6><a onClick = {() => this.props.onClick(8)}>Des Automne</a></h6>
+        <h4>Portfolio</h4>
+        <h6 onClick = {() => this.selectClick(0)} className = {this.state.selected == 0 ? 'selected' : ''} >
+          Festive Fall
+        </h6>
+        <h6 onClick = {() => this.selectClick(1)} className = {this.state.selected == 1 ? 'selected' : ''} >
+          Sequin Drip Jacket
+        </h6>
+        <h6 onClick = {() => this.selectClick(2)} className = {this.state.selected == 2 ? 'selected' : ''} >
+          Bratz Evening Wear
+        </h6>
+        <h6 onClick = {() => this.selectClick(3)} className = {this.state.selected == 3 ? 'selected' : ''} >
+          Remnant Fabric Collection In Process
+        </h6>
+        <h6 onClick = {() => this.selectClick(4)} className = {this.state.selected == 4 ? 'selected' : ''} >
+          Costume Designer: Hades and Persephone
+        </h6>
+        <h6 onClick = {() => this.selectClick(5)} className = {this.state.selected == 5 ? 'selected' : ''} >
+          Slow Fashion In Process
+        </h6>
+        <h6 onClick = {() => this.selectClick(6)} className = {this.state.selected == 6 ? 'selected' : ''} > 
+          Science Fiction Collection
+        </h6>
+        <h6 onClick = {() => this.selectClick(7)} className = {this.state.selected == 7 ? 'selected' : ''} >
+          Leaf Block Print
+        </h6>
+        <h6 onClick = {() => this.selectClick(8)} className = {this.state.selected == 8 ? 'selected' : ''} >
+          Des Automne
+        </h6>
 
       </div>
     );
